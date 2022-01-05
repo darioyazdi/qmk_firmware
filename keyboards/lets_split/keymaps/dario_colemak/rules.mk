da33aad86524ac9b8dd31f717e4186a517894290
Dario Yazdi <3846307+darioyazdi@users.noreply.github.com>
2022-01-05 11:31:30 -0800

    toggle arrow layer

--- keyboards/lets_split/keymaps/dario_colemak/rules.mk
+++ keyboards/lets_split/keymaps/dario_colemak/rules.mk
@@ -1,5 +1,3 @@
-SRC += ./generated_keymap.c
-
 MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
 NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
