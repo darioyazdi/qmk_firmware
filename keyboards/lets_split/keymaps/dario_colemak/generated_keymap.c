d81a82b731b55d1dc690ece828ffeb02f1e12628
Dario Yazdi <3846307+darioyazdi@users.noreply.github.com>
2022-01-05 00:08:52 -0800

    format

--- keyboards/lets_split/keymaps/dario_colemak/generated_keymap.c
+++ keyboards/lets_split/keymaps/dario_colemak/generated_keymap.c
@@ -11,19 +11,18 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_COLEMAK] = LAYOUT_ortho_4x12( \
-    KC_GESC, KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSPC, \
-    KC_LCTL, KC_A, KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, KC_O, KC_QUOT, \
-    LSFT_T(KC_LBRC), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_K, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_RBRC), \
-    MO(2), KC_NO, KC_NO, KC_LALT, KC_LGUI, LT(1,KC_SPC), LT(1,KC_SPC), KC_ENT, KC_BSLS, KC_NO, KC_NO, KC_NO),
-	[_NUMPAD] = LAYOUT_ortho_4x12( \
-    KC_GRV, KC_TRNS, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_7, KC_8, KC_9, KC_TRNS, KC_DEL, \
-    KC_TAB, KC_TRNS, KC_F5, KC_F6, KC_F7, KC_F8, KC_EQL, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS, \
-    KC_LBRC, KC_LPRN, KC_F1, KC_F2, KC_F3, KC_F4, KC_MINS, KC_1, KC_2, KC_3, KC_RPRN, KC_RBRC, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_TRNS, KC_TRNS, KC_TRNS), 
-	[_ARROW] = LAYOUT_ortho_4x12( \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET, KC_TRNS, \
-    KC_TRNS, KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
-};
+  [_COLEMAK] = LAYOUT_ortho_4x12(
+      KC_GESC         , KC_Q  , KC_W  , KC_F    , KC_P    , KC_G          , KC_J          , KC_L   , KC_U    , KC_Y   , KC_SCLN , KC_BSPC         ,
+      KC_LCTL         , KC_A  , KC_R  , KC_S    , KC_T    , KC_D          , KC_H          , KC_N   , KC_E    , KC_I   , KC_O    , KC_QUOT         ,
+      LSFT_T(KC_LBRC) , KC_Z  , KC_X  , KC_C    , KC_V    , KC_B          , KC_K          , KC_M   , KC_COMM , KC_DOT , KC_SLSH , RSFT_T(KC_RBRC) ,
+      MO(2)           , KC_NO , KC_NO , KC_LALT , KC_LGUI , LT(1, KC_SPC) , LT(1, KC_SPC) , KC_ENT , KC_BSLS , KC_NO  , KC_NO   , KC_NO) ,
+  [_NUMPAD] = LAYOUT_ortho_4x12(
+    KC_GRV  , KC_TRNS , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_TRNS , KC_7    , KC_8 , KC_9    , KC_TRNS , KC_DEL   ,
+    KC_TAB  , KC_TRNS , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_EQL  , KC_4    , KC_5 , KC_6    , KC_TRNS , KC_TRNS  ,
+    KC_LBRC , KC_LPRN , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_MINS , KC_1    , KC_2 , KC_3    , KC_RPRN , KC_RBRC  ,
+    KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_0 , KC_TRNS , KC_TRNS , KC_TRNS) ,
+  [_ARROW] = LAYOUT_ortho_4x12(
+    KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , RESET      ,
+    KC_TRNS , KC_TRNS , KC_MPRV , KC_MPLY , KC_MNXT , KC_TRNS , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , KC_TRNS , KC_TRNS    ,
+    KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS    ,
+    KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS)};
