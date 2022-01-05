3ad02a6cbc64abce6498baf3515c859871604ef9
Dario Yazdi <3846307+darioyazdi@users.noreply.github.com>
2022-01-04 23:47:49 -0800

    wip

--- keyboards/lets_split/keymaps/dario_colemak/rules.mk
+++ keyboards/lets_split/keymaps/dario_colemak/rules.mk
@@ -1,1 +1,6 @@
 SRC += ./generated_keymap.c
+
+MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+COMMAND_ENABLE = no        # Commands for debug and configuration
